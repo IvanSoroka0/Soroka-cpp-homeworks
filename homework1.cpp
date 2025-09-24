27c622bd9d58e53a50df970078b592bb989333d6
IvanSoroka0 <ivannersor@gmail.com>
2025-09-24 19:40:58 +0500

    Update homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,9 +1,13 @@
-
+#include <string>
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout<<"Hello World";
-
-    return 0;
+    std::string name;
+    std::cout<<"Введите ваше имя: ";
+    std::cin>>name;
+    std::cout<<"Я всепочтенно, со всем своим великодушием и скромностью, приветствую вас, "<<name<<".";
+    
 }
