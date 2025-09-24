da8d174fd0cb510b3531c26005fe3cdec0217e26
IvanSoroka0 <ivannersor@gmail.com>
2025-09-24 19:52:58 +0500

    Add files via upload

---
+++ homework1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::string name;
+    std::cout<<"Введите ваше имя: ";
+    std::cin>>name;
+    std::cout<<"Я всепочтенно, со всем своим великодушием и скромностью, приветствую вас, "<<name<<".";
+
+    
+}
