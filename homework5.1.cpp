b7e1e60b4860e94bc609273c07522fd33778d6cb
IvanSoroka0 <ivannersor@gmail.com>
2025-10-22 18:15:30 +0500

    Update and rename homework5.1 to homework5.1.cpp

---
+++ homework5.1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    double r, s;
+    
+    // Спрашиваем радиус
+    cout << "Введите радиус круга: ";
+    cin >> r;
+    
+    // Вычисление площади круга
+    s = 3,14 * radius * radius;
+    
+    // Вывод результата
+    cout << "Площадь круга с радиусом " << radius << " равна: " << area << endl;
+    
+    return 0;
+}
