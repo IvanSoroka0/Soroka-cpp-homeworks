239d82e1f194cbb1452d281d2216c3b4fa0f864f
IvanSoroka0 <ivannersor@gmail.com>
2025-10-22 18:28:03 +0500

    Create homework5.2.cpp

---
+++ homework5.2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+      //Вариант 1
+    int year;
+    
+    // Проверка високосного года
+    bool vis = false;
+    
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                vis = true;
+            }
+        } else {
+            vis = true;
+        }
+    }
+    
+    // Вывод результата
+    if (vis == true) {
+        cout << year << " год является високосным." << endl;
+    } else {
+        cout << year << " год не является високосным." << endl;
+    }
+    
+    return 0;
+}
